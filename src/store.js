7467b9ba33b7469e65e604fb6896fbe52b067488
mohan <mohan.talachutla@gmail.com>
2024-12-01 00:04:02 +0530

    inject store

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import {
+  createReduxStore,
+  createInjectReducer,
+  createInjectSaga,
+} from "@mohantalachutla/inject-store";
+// import rootReducer from "../reducers";
+// import rootSaga from "../sagas";
+import createSagaMiddleware from "redux-saga";
+
+//sage middleware
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createReduxStore({ middlewares: [sagaMiddleware] });
+const { injectReducers } = createInjectReducer(store);
+const { injectSaga } = createInjectSaga(store, sagaMiddleware.run);
+
+//injecting reducers and sagas
+// injectReducers(rootReducer);
+// injectSaga("rootSaga", rootSaga);
+
+console.info({ store });
+export default store;
