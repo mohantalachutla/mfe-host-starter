ae81c33bf914c8b599d452da93139901bfdc8251
mohan <mohan.talachutla@gmail.com>
2024-12-03 02:29:10 +0530

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+import { storybookWebpackConfig } from "../scripts/storybook";
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  webpackFinal: storybookWebpackConfig,
+};
+export default config;
