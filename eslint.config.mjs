a5192122f9e5c85acf8605ed22dc66d547107ad4
mohan <mohan.talachutla@gmail.com>
2024-12-09 04:42:49 +0530

    eslint react plugin: recommended

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import babelParser from '@babel/eslint-parser';
+import pluginReact from '@eslint-react/eslint-plugin';
 
 const ignores = ['**/node_modules/', '**/lib/', '**/dist/'];
 /** @type {import('eslint').Linter.Config[]} */
@@ -25,6 +26,29 @@ export default [
     },
     rules: {},
   },
-  pluginJs.configs.recommended,
-  // pluginJs.configs.all
+  {
+    files: ['**/*.{js, json}'],
+    ...pluginJs.configs.recommended,
+    // pluginJs.configs.all,
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    ...pluginReact.configs.recommended,
+    languageOptions: {
+      parser: babelParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parserOptions: {
+        requireConfigFile: false,
+        allowImportExportEverywhere: false,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      '@eslint-react/no-prop-types': 'off',
+    },
+  },
 ];
