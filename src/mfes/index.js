d5a0b86ec8a24d81ad963eedb3b0816ca3e80caf
mohan <mohan.talachutla@gmail.com>
2024-12-29 22:33:09 +0530

    fix order mfe config

--- src/mfes/index.js
+++ src/mfes/index.js
@@ -1,6 +1,6 @@
 import mfeProduct from './mfe-product';
 import mfeOrder from './mfe-order';
-import mfeWishlist from './mfe-order';
+import mfeWishlist from './mfe-wishlist';
 import mfeCart from './mfe-cart';
 
 export { mfeProduct, mfeOrder, mfeWishlist, mfeCart };
